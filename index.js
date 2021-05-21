30d6ee9c67c40d56fa396624a1bb86e507d59522
precious123-gifted <58713062+precious123-gifted@users.noreply.github.com>
2021-05-21 22:30:52 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function convertFahrToCelsius(fahrenheit) {
   var fahrenheit = Input.value;
   var fTemp = fahrenheit;
   var fToCel = ((fTemp - 32) * 5) / 9;
-  var message = fTemp + "\xB0F is " + fToCel + "\xB0C.";
+  var message = fTemp + "\xB0F is " + fToCel.toFixed(4) + "\xB0C.";
 
   Answer.innerHTML = fToCel.toFixed(4) + "\xB0C.";
 
